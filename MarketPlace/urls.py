f842fa8e114aeb76ccb228989adff508352a72fa
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-05-30 14:11:40 +0430

    rename home file

--- MarketPlace/urls.py
+++ MarketPlace/urls.py
@@ -19,5 +19,5 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.index, name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'core/home.html', {})
+    return render(request, 'core/index.html', {})
