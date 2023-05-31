ca4b22bf0b000372b98cbdbc8099d5313881e452
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-05-31 14:34:58 +0430

    page customization

--- MarketPlace/urls.py
+++ MarketPlace/urls.py
@@ -19,5 +19,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'), 
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html', {})
+
+def contact(request):
+    return render(request, 'core/contact.html', {})
