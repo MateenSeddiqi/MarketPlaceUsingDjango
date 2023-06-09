5d87383ccfad48c94ea6911d0909f0af45bc5a8e
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-06-10 01:54:22 +0430

    model changes

--- item/models.py
+++ item/models.py
@@ -23,3 +23,8 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+# This code is defining a foreign key relationship between two models in Django.
+# The first argument, Category, specifies the model that this foreign key is relating to. The related_name argument specifies the name of the reverse relation from the related object back to this one. 
+# In this case, it's specifying that each Category object should have a related manager named 'items' that can be used to retrieve all related Item objects. Finally, on_delete specifies what should happen when the related object is deleted. In this case, it's set to models.CASCADE,
+# which means that when a Category object is deleted, all related Item objects will also be deleted.
