1440505121863c17232fda453052d49a0b9d28d4
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-06-11 01:44:31 +0430

    SignUp

--- core/form.py
+++ core/form.py
@@ -5,4 +5,24 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     class Meta:
         model=User
-        fields=('username', 'email', 'password', 'password2')
+        fields=('username', 'email', 'password1', 'password2')
+
+    username=forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Your username',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email=forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder':'Your email',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Password',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confirm Password',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
 from  .form import SignupForm
 
@@ -15,8 +15,16 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html', {})
 
+
 def signup(request):
-    form=SignupForm()
+    if request.method =='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:  
+        form=SignupForm()
+
     return render(request, 'core/signup.html', {
         'form':form 
     })
