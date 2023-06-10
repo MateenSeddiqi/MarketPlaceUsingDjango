0dea878653e9b82c159fee6148773cb56f9b7e09
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-06-10 19:39:29 +0430

    Details page changes

--- item/views.py
+++ item/views.py
@@ -3,6 +3,8 @@ from .models import Item
 
 def details(request, pk):
     item=get_object_or_404(Item, pk=pk)
+    related_items=Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
     return render (request, 'item/details.html', {
-        'item':item
+        'item':item,
+        'related_items':related_items,
     })
