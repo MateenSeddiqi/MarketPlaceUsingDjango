80784a41cf76a7469bfbfd18a49cd52fe2c6f58d
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-06-10 17:14:50 +0430

    data displaied from database to the view

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html', {})
+    categories=Category.objects.all()
+    return render(request, 'core/index.html', {'categories':categories})
 
 def contact(request):
     return render(request, 'core/contact.html', {})
