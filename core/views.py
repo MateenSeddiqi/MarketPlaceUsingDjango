f912765b4842b2e2ad8d219f00263c4b9146594b
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-06-10 17:20:24 +0430

    items

--- core/views.py
+++ core/views.py
@@ -3,8 +3,13 @@ from item.models import Category, Item
 
 # Create your views here.
 def index(request):
+    items=Item.objects.filter(is_sold=False)[0:6]
     categories=Category.objects.all()
-    return render(request, 'core/index.html', {'categories':categories})
+
+    return render(request, 'core/index.html', {
+        'categories':categories,
+        'items': items,    
+    })
 
 def contact(request):
     return render(request, 'core/contact.html', {})
