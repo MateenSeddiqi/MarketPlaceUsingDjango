2f4c3324642eee2d8211483806087cf241f9faf3
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-06-13 22:09:00 +0430

    dashboard app completed

--- MarketPlace/urls.py
+++ MarketPlace/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('item/', include('item.urls')),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
