fa97fd1fdd7b81bc793d6b49f2286419051062b6
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-07-09 14:40:04 +0430

    fix the error

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ app_name='core'
 
 urlpatterns=[
 
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login')
