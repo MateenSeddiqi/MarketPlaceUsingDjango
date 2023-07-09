bf6ff792a3ea5c5d41bdd23096b9113455e0b118
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-07-09 14:37:26 +0430

    change for upgrade

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from .forms import LoginForm
 app_name='core'
 
 urlpatterns=[
-    path('', views.index, name='index'),
+
+    path('index/', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login')
