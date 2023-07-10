d681e5898eca4473b65c70bb662708df40ad1f51
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-07-10 14:49:28 +0430

    logout

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,7 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='logout'),
 ]
+
