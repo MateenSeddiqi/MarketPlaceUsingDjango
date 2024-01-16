8fa35399f85e42ec3ada3efcab35abc131133c0d
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-01-16 23:32:58 +0430

    Item Form changes

--- item/views.py
+++ item/views.py
@@ -41,14 +41,14 @@ def delete(request, pk):
 def EditItem(request, pk):
     item =get_object_or_404(Item, pk=pk, created_by=request.user)
     if request.method=='POST':
-        form=EditItemForm(request.POST, request.FILES, instance=item)
-        if form.is_valid():
-            form.save()
+        editItemForm=EditItemForm(request.POST, request.FILES, instance=item)
+        if editItemForm.is_valid():
+            editItemForm.save()
             return redirect('item:details', pk=item.id)
     else:
-        form = EditItemForm(instance=item)
+        editItemForm = EditItemForm(instance=item)
     return render(request, 'item/form.html', {
-        'form':form,
+        'editItemForm':editItemForm,
         'title': 'Edit item',
     })
 
